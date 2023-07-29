19631e670ec5040156d00f678be56d4fa67d993d
Michael Cassano <mcassano@gmail.com>
2023-07-29 10:28:24 -0600

    labels

--- app/controllers/playdate_controller.rb
+++ app/controllers/playdate_controller.rb
@@ -2,7 +2,7 @@ class PlaydateController < ApplicationController
     include CourseHelper
     def show
       @date = params[:date]
-      @output = denver_fetch_and_filter(params[:date])
+      @city_of_denver = denver_fetch_and_filter(params[:date])
     end
   end
   
